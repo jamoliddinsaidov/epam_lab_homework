cd75ae1f5ab59fddfadc7ea1f78cb974fb15c4ae
sjamoliddin <sjamolbek@mail.ru>
2021-07-14 17:44:28 +0500

    fix category btns

--- react/08_tvcafe/src/pages/Home/Home.js
+++ react/08_tvcafe/src/pages/Home/Home.js
@@ -2,14 +2,15 @@ import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 
 // components
-import Search from '../../components/Search/Search'
+// import Search from '../../components/Search/Search'
 import HeaderTitle from '../../components/Titles/HeaderTitle'
 import MovieCarousel from '../../components/MovieCarousel/MovieCarousel'
 import HomeMovieList from '../../components/MovieLists/HomeMovieList'
 import Footer from '../../components/Footer/Footer'
+import HomeMovieCategoryContainer from './HomeMovieCategoryContainer'
 
 // utils
-import { colors, Container } from '../../components/GlobalStyles'
+// import { colors, Container } from '../../components/GlobalStyles'
 
 // redux
 import { useDispatch, useSelector } from 'react-redux'
@@ -47,29 +48,19 @@ const Home = () => {
 			{/* <Search placeholder='Search movies by name...' /> */}
 
 			{!isLoading && (
-				<div>
-					<HeaderTitle title='Today on TV' className='h2' />
-					<MovieCarousel movies={scheduledForToday} />
-				</div>
-			)}
-
-			<StyledHomeMovieCategoryContainer>
-				<div>
-					<HeaderTitle title='we offer' />
-				</div>
-				<div className='line'></div>
-				<StyledHomeOptions>
-					<button>Popular Shows</button>
-					<button>Animations</button>
-					<button>Help Finding Favorites</button>
-				</StyledHomeOptions>
-			</StyledHomeMovieCategoryContainer>
-
-			{!isLoading && (
-				<HomeMovieList
-					movies={popularShowsState}
-					loadMoreHandler={loadMoreHandler}
-				/>
+				<>
+					<div>
+						<HeaderTitle title='Today on TV' className='h2' />
+						<MovieCarousel movies={scheduledForToday} />
+					</div>
+					<div>
+						<HomeMovieCategoryContainer />
+						<HomeMovieList
+							movies={popularShowsState}
+							loadMoreHandler={loadMoreHandler}
+						/>
+					</div>
+				</>
 			)}
 
 			<Footer />
@@ -82,38 +73,4 @@ const StyledHome = styled.div`
 	position: relative;
 `
 
-const StyledHomeMovieCategoryContainer = styled.div`
-	background: ${colors.bgNavColor};
-	padding: 1.5em 0;
-`
-const StyledHomeOptions = styled(Container)`
-	padding: 1.5em 0 0.5em;
-
-	button {
-		margin-right: 1em;
-		text-transform: uppercase;
-		font-weight: 300;
-		font-size: 1.2rem;
-		cursor: pointer;
-		border-radius: 0;
-		border: none;
-		background: transparent;
-		outline: none;
-		font-family: 'Source Sans Pro', sans-serif;
-		border-bottom: 1px solid transparent;
-		transition: 300ms ease-in-out;
-		transition-property: color, padding, border-color;
-
-		&:hover,
-		&:focus,
-		&.active {
-			color: ${colors.primaryColorTwo};
-		}
-
-		&.active {
-			border-color: ${colors.primaryColorTwo};
-			padding: 0 0.5em;
-		}
-	}
-`
 export default Home
---
+++ react/08_tvcafe/src/pages/Home/HomeMovieCategoryContainer.js
@@ -0,0 +1,113 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+// components
+import HeaderTitle from '../../components/Titles/HeaderTitle'
+
+// utils
+import { colors, Container } from '../../components/GlobalStyles'
+
+const HomeMovieCategoryContainer = () => {
+	// states
+	const [isActive, setIsActive] = useState({
+		popularBtn: true,
+		animationBtn: false,
+		helpBtn: false,
+	})
+
+	// handlers
+	const clickHandler = (e) => {
+		const text = e.target.innerHTML
+		switch (text) {
+			case 'popular shows':
+				setIsActive((prevState) => ({
+					...prevState,
+					popularBtn: true,
+					animationBtn: false,
+					helpBtn: false,
+				}))
+				break
+			case 'animations':
+				setIsActive((prevState) => ({
+					...prevState,
+					popularBtn: false,
+					animationBtn: true,
+					helpBtn: false,
+				}))
+				break
+			case 'help finding favorites':
+				setIsActive((prevState) => ({
+					...prevState,
+					popularBtn: false,
+					animationBtn: false,
+					helpBtn: true,
+				}))
+				break
+			default:
+				return isActive
+		}
+	}
+
+	return (
+		<StyledHomeMovieCategoryContainer>
+			<div>
+				<HeaderTitle title='we offer' />
+			</div>
+			<div className='line'></div>
+			<StyledHomeOptions>
+				<button
+					onClick={clickHandler}
+					className={isActive.popularBtn ? 'active' : ''}>
+					popular shows
+				</button>
+				<button
+					onClick={clickHandler}
+					className={isActive.animationBtn ? 'active' : ''}>
+					animations
+				</button>
+				<button
+					onClick={clickHandler}
+					className={isActive.helpBtn ? 'active' : ''}>
+					help finding favorites
+				</button>
+			</StyledHomeOptions>
+		</StyledHomeMovieCategoryContainer>
+	)
+}
+
+const StyledHomeMovieCategoryContainer = styled.div`
+	background: ${colors.bgNavColor};
+	padding: 1.5em 0;
+`
+const StyledHomeOptions = styled(Container)`
+	padding: 1.5em 0 0.5em;
+
+	button {
+		margin-right: 1em;
+		text-transform: uppercase;
+		font-weight: 300;
+		font-size: 1.2rem;
+		cursor: pointer;
+		border-radius: 0;
+		border: none;
+		background: transparent;
+		outline: none;
+		font-family: 'Source Sans Pro', sans-serif;
+		border-bottom: 1px solid transparent;
+		transition: 300ms ease-in-out;
+		transition-property: color, padding, border-color;
+
+		&:hover,
+		&:focus,
+		&.active {
+			color: ${colors.primaryColorTwo};
+		}
+
+		&.active {
+			border-color: ${colors.primaryColorTwo};
+			padding: 0 0.5em;
+		}
+	}
+`
+
+export default HomeMovieCategoryContainer
