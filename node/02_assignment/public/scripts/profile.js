638f270b6ca808e49397a20c0626c029ff36b9c3
sjamoliddin <sjamolbek@mail.ru>
2021-08-28 22:04:33 +0500

    add profile delete func

--- node/02_assignment/public/scripts/profile.js
+++ node/02_assignment/public/scripts/profile.js
@@ -2,7 +2,9 @@ const usernameField = document.querySelector('.username')
 const date = document.querySelector('.date')
 const userId = document.querySelector('.userId')
 const profileAlert = document.querySelector('.profile_warning')
+const deleteBtn = document.querySelector('.delete')
 
+// event listeners
 window.addEventListener('DOMContentLoaded', async () => {
 	try {
 		const token = localStorage.getItem('token')
@@ -21,6 +23,25 @@ window.addEventListener('DOMContentLoaded', async () => {
 	}
 })
 
+deleteBtn.addEventListener('click', async () => {
+	try {
+		const token = localStorage.getItem('token')
+		await axios.delete('/api/users/me', {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		})
+
+		localStorage.removeItem('token')
+
+		//redirect to dashboard
+		window.location.href = '/'
+	} catch (error) {
+		profileAlert.innerText = error
+	}
+})
+
+// functions
 function formatDate(date) {
 	date = new Date(date)
 	const options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' }
